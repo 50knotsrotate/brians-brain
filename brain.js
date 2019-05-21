3842e0965d2ac93d384b27484d66acdee837ab9f
= <=>
2019-05-21 07:55:06 -0700

    Just a bit of cleanup

--- brain.js
+++ brain.js
@@ -5,21 +5,25 @@ var grid = [];
 
 class Cell {
   constructor(x, y, pos) {
+    //x and y are needed only for drawing squares
     this.x = x;
     this.y = y;
     this.size = canvas.width / rows;
     //possible states 0, 1, dying
-      this.state = Math.random() > .35
+    this.state = Math.random() > 0.35;
+    //Need new state because I need to
+    //take a "snapshot" of all cells new status
+    //before updating them
     this.newState = null;
-    this.pos = pos
+    //We need pos to keep track of a cells "spot" in the grid
+    this.pos = pos;
 
     this.getNeighbors = function() {
       var count = 0;
       for (let i = -1; i < 2; i++) {
         for (let j = -1; j < 2; j++) {
-          if (grid[(this.pos.x + j + rows) % rows][(this.pos.y + i + rows) % rows].state === true) {
-            count++;
-          }
+          grid[(this.pos.x + i + rows) % rows][(this.pos.y + j + rows) % rows]
+            .state === true && count++;
         }
       }
       return count;
@@ -43,7 +47,10 @@ class Cell {
     let _temp = [];
     for (let j = 0; j < rows; j++) {
       _temp.push(
-        new Cell(j * (canvas.width / rows), i * (canvas.height / rows), {x: i, y: j})
+        new Cell(j * (canvas.width / rows), i * (canvas.height / rows), {
+          x: i,
+          y: j
+        })
       );
     }
     grid.push(_temp);
@@ -68,38 +75,23 @@ function drawGrid() {
         grid[j][i].size
       );
       context.fill();
-       context.stroke();
+      context.stroke();
     }
   }
 }
-
-// canvas.addEventListener('click', () => { 
-//   for (let i = 0; i < rows; i++) {
-//     for (let j = 0; j < rows; j++) {
-//       grid[j][i].update();
-//     }
-//   }
-
-//   for (let i = 0; i < rows; i++) {
-//     for (let j = 0; j < rows; j++) {
-//       grid[j][i].state = grid[j][i].newState;
-//       grid[j][i].newState = null;
-//     }
-//   }
-// })
 
 setInterval(() => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < rows; j++) {
       grid[j][i].update();
     }
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[j][i].state = grid[j][i].newState;
+      grid[j][i].newState = null;
     }
-    
-    for (let i = 0; i < rows; i++) { 
-        for (let j = 0; j < rows; j++) { 
-            grid[j][i].state = grid[j][i].newState
-            grid[j][i].newState = null
-        }
-    }
+  }
   drawGrid();
 }, 50);
